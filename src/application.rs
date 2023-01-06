ba8914327fd55b761778fb3cd2e1a8e2e590ff92
eriley <me@eoghanriley.dev>
2023-01-06 16:41:23 -0500

    Rewrote the entry point

--- src/application.rs
+++ src/application.rs
@@ -1,7 +1,12 @@
-pub fn run() {
-    loop {
-        while true {
+pub trait Application {
+    fn run() {
+        loop {
             continue;
         }
+    }
+
+    fn init() -> (Vec<crate::event::EventTypes>) {
+        crate::logger::init();
+        crate::event::init()
     }
 }
